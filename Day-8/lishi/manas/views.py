2f360624159c3e4829f20037f3c4215769c10fbc
kodurubhavyasree <kbhavyasree2003@gmail.com>
2025-06-27 16:03:33 +0530

    Add files via upload

---
+++ Day-8/lishi/manas/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def bhavya(request):
+    return HttpResponse("manas,django!")
+def bhavya(requst):
+    return HttpResponse("manas,monday!")
+# Create your views here.
